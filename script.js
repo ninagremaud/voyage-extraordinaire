c6d79f0959ead1832308fe28a8f724df7f2182f4
ninagremaud <nani.grem@hotmail.com>
2026-08-05 08:17:58 +0200

    Add functionality for start and unlock buttons

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const startButton=document.getElementById("startButton");
 
+const cover=document.getElementById("cover");
+
+const chapter=document.getElementById("chapter");
+
+const unlockButton=document.getElementById("unlockButton");
+
+const password=document.getElementById("password");
+
+const message=document.getElementById("message");
+
+startButton.onclick=()=>{
+
+cover.style.display="none";
+
+chapter.classList.remove("hidden");
+
+}
+
+unlockButton.onclick=()=>{
+
+if(password.value.toLowerCase()=="shire"){
+
+message.innerHTML="✨ Le premier chapitre sera bientôt révélé.";
+
+message.style.color="green";
+
+}else{
+
+message.innerHTML="Le Livre Rouge demeure silencieux...";
+
+message.style.color="darkred";
+
+}
+
+}
